ac69ae7bc905518cd5ced8cba449d36d57a4a932
GauravSaini8290 <sainigauravkumar186@gmail.com>
2025-12-24 14:58:29 +0530

    Remove database
    Removed database connection code from the config file.

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,1 @@
-const mongoose = require("mongoose");
 
-const connectDB = async () => {
-  await mongoose.connect(
-    "mongodb+srv://sainigauravkumar186_db_user:ZGDcmxQqcd6rzadW@namsatenodejs.yrfv2ld.mongodb.net/devtender"
-  );
-};
-
-module.exports = connectDB;
